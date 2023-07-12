fbb863021a7b517b01e1e2415003ad9074152159
JiriSvacek <svacek.jiri@email.cz>
2023-07-12 23:34:51 +0200

    Translation

--- web_scrape.py
+++ web_scrape.py
@@ -4,95 +4,100 @@ import re
 import csv
 import sys
 
-UrlZaklad = "https://volby.cz/pls/ps2017nss/"
-zahlavi = ["kód obce",
-            "název obce",
-            "voliči v seznamu",
-            "vydané obálky",
-            "platné hlasy"
-            ]
+UrlBasic = "https://volby.cz/pls/ps2017nss/"
+header_data = ["kód obce",
+               "název obce",
+               "voliči v seznamu",
+               "vydané obálky",
+               "platné hlasy"
+               ]
 
-obsah = list()
-okresR = str()
-soubor = str()
+content = list()
+countyR = str()
+file = str()
 data = list()
-chyba = False
+error = False
 
-def tocsv():
-    global soubor
-    f = open(soubor, "w")
+
+def to_csv():
+    global file
+    f = open(file, "w")
     f_write = csv.writer(f)
-    f_write.writerow(zahlavi)
-    f_write.writerows(obsah)
+    f_write.writerow(header_data)
+    f_write.writerows(content)
     f.close()
 
-def find(obecS, args):
+
+def find(countyS, args):
     global data
-    for i in obecS.findAll("td", attrs=args):
+    for i in countyS.findAll("td", attrs=args):
         data.append(i.get_text())
 
-def nazev(obecS):
-    global data
-    nazev = obecS.find("h3", string=re.compile("Obec"), class_=None).get_text()
-    data.append(nazev[7:].strip("\n"))
 
-def nadpis(obecS):
-    global zahlavi
-    for strana in obecS.findAll("td", class_="overflow_name"):
-        zahlavi.append(strana.get_text())
-
-def obce(okresS):
-    global chyba
-    global obsah
+def title(cityS):
     global data
-    prvni = True
-    for link in okresS.findAll("td", class_="cislo"):
+    name = cityS.find("h3", string=re.compile('Obec'), class_=None).get_text()
+    data.append(name[7:].strip("\n"))
+
+
+def header(cityS):
+    global header_data
+    for strana in cityS.findAll("td", class_="overflow_name"):
+        header_data.append(strana.get_text())
+
+
+def county(countyS):
+    global error
+    global content
+    global data
+    first = True
+    for link in countyS.findAll("td", class_="cislo"):
         data = list()
         data.append(link.string)
-        obecR = requests.get(UrlZaklad + link.a["href"])
-        if obecR.status_code != 200:
-            chyba = True
-        obecS = bs4.BeautifulSoup(obecR.text, "html.parser")
-        if prvni:
-            nadpis(obecS)
-        prvni = False
-        nazev(obecS)
-        find(obecS, {"data-rel": "L1", "headers": ["sa2", "sa3", "sa6"]})
-        find(obecS, {"class": "cislo", "headers": ["t1sa2 t1sb3", "t2sa2 t2sb3"]})
-        obsah.append(data)
+        city_r = requests.get(UrlBasic + link.a["href"])
+        if city_r.status_code != 200:
+            error = True
+        city_s = bs4.BeautifulSoup(city_r.text, "html.parser")
+        if first:
+            header(city_s)
+        first = False
+        title(city_s)
+        find(city_s, {"data-rel": "L1", "headers": ["sa2", "sa3", "sa6"]})
+        find(city_s, {"class": "cislo", "headers": ["t1sa2 t1sb3", "t2sa2 t2sb3"]})
+        content.append(data)
 
-def overeni():
-    global okresR
-    global soubor
-    okresR = requests.get(sys.argv[1])
-    if okresR.status_code != 200:
-        print("Špatné URL/chybový kod:", okresR.status_code)
+
+def check() -> bool:
+    """Checks if user entered valid url and filename"""
+    global countyR
+    global file
+    countyR = requests.get(sys.argv[1])
+    if countyR.status_code != 200:
+        print("Wrong URL/error code:", countyR.status_code)
         print(sys.argv[1])
         return False
     else:
         if sys.argv[2].endswith(".csv"):
-            print("Url i název souboru v pořádku")
-            soubor = sys.argv[2]
+            print("Url and filename OK")
+            file = sys.argv[2]
             return True
         elif sys.argv[2].find(".") == int(-1):
-            print("Url i název souboru v pořádku (přidána přípona)")
-            soubor = sys.argv[2] + ".csv"
+            print("Url and filename OK (added suffix)")
+            file = sys.argv[2] + ".csv"
             return True
         else:
-            print("Špatnný název souboru/přípony")
+            print("Wrong filename/suffix")
             return False
 
-def main():
-    if not overeni():
-        exit()
-    okresS = bs4.BeautifulSoup(okresR.text, "html.parser")
-    print("Začínám se scrapingem...")
-    obce(okresS)
-    if chyba:
-        print("K některým obcím se nepodařilo připojit")
-    print("Převádím do csv", soubor)
-    tocsv()
-    print("Hotovo")
 
 if __name__ == "__main__":
-    main()
+    global error
+    if not check():
+        exit()
+    print("Start with scraping...")
+    county(bs4.BeautifulSoup(countyR.text, "html.parser"))
+    if error:
+        print("Could not connect to some cities")
+    print("Convert to csv", file)
+    to_csv()
+    print("Done")
